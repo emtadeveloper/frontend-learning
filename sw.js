e365d377ab22cb412cb1d17d7e0e622c9933af61
emtadeveloper <emta137671@gmail.com>
2025-01-22 11:05:36 +0330

    sdfsdf

--- sw.js
+++ sw.js
@@ -12,6 +12,19 @@ let staticItems = [
 let STATIC_CHACHE = "static-v1";
 let DYNAMIC_CHACHE = "dynamic-v1";
 
+const trimCache = (chachName, max) => {
+  caches.open(chachName).then((cache) => {
+    return cache.keys().then((keys) => {
+      console.log(keys);
+      if (keys.length > max) {
+        cache.delete(keys[0]).then(() => {
+          trimCache(chachName, max);
+        });
+      }
+    });
+  });
+};
+
 self.addEventListener("install", function (e) {
   //  caches کردن اولیه فایل هامون
 
@@ -42,26 +55,70 @@ self.addEventListener("activate", function (e) {
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return (
-        res ||
-        fetch(e.request)
-          .then((fetchRes) => {
-            return caches.open(DYNAMIC_CHACHE).then((cache) => {
-              // cach.put اضافه کردن یک مقدار جدید
-              cache.put(e.request, fetchRes.clone());
-              return fetchRes;
-            });
-          })
-          .catch((err) => {
-            return caches.open(STATIC_CHACHE).then((cache) => {
-              if (e.request.headers.get("accept").includes("text/html")) {
-                return cache.match("offline.html");
-              }
-            });
-          })
-      );
-    })
-  );
+  if (e.request.url.indexOf("https://jsonplaceholder.typicode.com/users") > -1) {
+    e.responseWith(
+      caches.open(DYNAMIC_CHACHE).then((cache) => {
+        return fetch(e.request).then((response) => {
+          trimCache(DYNAMIC_CHACHE, 5);
+          cache.put(e.request, response.clone());
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((res) => {
+        return (
+          res ||
+          fetch(e.request)
+            .then((fetchRes) => {
+              return caches.open(DYNAMIC_CHACHE).then((cache) => {
+                // cach.put اضافه کردن یک مقدار جدید
+                trimCache(DYNAMIC_CHACHE, 5);
+                cache.put(e.request, fetchRes.clone());
+                return fetchRes;
+              });
+            })
+            .catch((err) => {
+              return caches.open(STATIC_CHACHE).then((cache) => {
+                if (e.request.headers.get("accept").includes("text/html")) {
+                  return cache.match("/offline.html");
+                }
+              });
+            })
+        );
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclick", (event) => {
+  if (event.action == "confrim") {
+    console.log("اکشن مورد نظر تایید شد");
+  } else if (event.action === "cancel") {
+    console.log("اکشن مورد نظر نادیده گرفته شد");
+  } else {
+    console.log("اکشنی انتخاب نشد");
+  }
+});
+
+self.addEventListener("push", (event) => {
+  const notification = event.data.json();
+  const options = {
+    body: notification.body,
+    icon: "/assets/images/codeyadIcon.png",
+    image: "/assets/images/office.jpg",
+    dir: "ltr",
+    vibrate: [100, 50, 200],
+    badge: "/assets/images/codeyadIcon.png",
+    tag: "group1",
+    renotify: true,
+    actions: [
+      { action: "confirm", title: "تایید", icon: "/assets/images/confirm.png" },
+      { action: "cancel", title: "انصراف", icon: "/assets/images/cancel.png" },
+    ],
+    data: {
+      notifUrl: notification.url,
+    },
+  };
+  self.registration.showNotification(notification.title, options);
 });
